9850fd56ca7630b46921d503f3db6d25924aa8d1
Thaís <parisethais@gmail.com>
2025-12-17 01:43:07 -0300

    solution

--- user/urls.py
+++ user/urls.py
@@ -6,5 +6,6 @@ app_name = "user"
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
     path("login/", CreateTokenView.as_view(), name="login"),
+    path("token/", CreateTokenView.as_view(), name="token"),
     path("me/", ManageUserView.as_view(), name="manage"),
 ]
